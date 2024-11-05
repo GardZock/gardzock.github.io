6375045911ab412f5661dc7a794aedf51eb8efe1
GardZock <miguellele2008@hotmail.com>
2024-11-04 22:45:22 -0300

    first appear

--- js/main.js
+++ js/main.js
@@ -4,15 +4,17 @@ Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-    faceapi.nets.ageGenderNet.loadFromUri('/models')
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
 ]).then(startVideo);
 
-function startVideo() {
-    navigator.getUserMedia(
-        { video: {} },
-        stream => (video.srcObject = stream),
-        err => console.error(err)
+async function startVideo() {
+    const stream = await navigator.mediaDevices.getUserMedia(
+        { video: true }
     );
+    video.srcObject = stream
+    video.play()
 }
 
 // Add event listener for video playing
