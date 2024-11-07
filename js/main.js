5186e3b82793986e609c4129301e058319d85ddb
GardZock <miguellele2008@hotmail.com>
2024-11-07 07:55:24 -0300

    fixes

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@ async function startVideo() {
     const constraints = {
         video: {
             facingMode: "user",
-            width: { min: 640, ideal: 1280, max: 1920 },
-            height: { min: 480, ideal: 720, max: 1080 }
+            width: { min: 640, ideal: 1280, max: 1280 },
+            height: { min: 480, ideal: 720, max: 720 }
         }
     };
 
